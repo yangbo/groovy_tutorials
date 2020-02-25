8081f4832e61645c68ba33cadba789f2c8a7db7e
bo.yang <bo.yang@telecwin.com>
2020-02-25 16:11:42 +0800

    设置先编译groovy代码，用groovy编译器统一编译groovy和java

---
+++ src/main/groovy/com/telecwin/groovy/tutorials/User.groovy
@@ -0,0 +1,8 @@
+package com.telecwin.groovy.tutorials
+
+class User {
+    String name
+    def useMain(){
+        Main main = new Main()
+    }
+}
---
+++ src/main/java/com/telecwin/groovy/tutorials/Main.java
@@ -0,0 +1,12 @@
+package com.telecwin.groovy.tutorials;
+
+public class Main {
+    public Main(){
+        User user = new User();
+        user.setName("yangbo");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
